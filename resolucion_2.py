874da4ea986f674feb3f72faca29eb3f539aad44
bpace1 <82056912+bpace1@users.noreply.github.com>
2024-10-28 11:32:06 -0300

    final solution

--- resolucion_2.py
+++ resolucion_2.py
@@ -1,54 +1,37 @@
-import os
+import os 
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import Dict, List, Tuple, Any
 
+
 PATH_IMAGENES: str = os.path.join(os.getcwd(), "data")
-
-row_height: int = 15
-
 Matlike = np.ndarray
 
-mapa_subconjuntos: Dict[str, Dict[str, Tuple[int, int]]] = {
-    "nombre": (),
-    "date": (),
-    "class": (),
-    "1": (),
-    "2": (),
-    "3": (),
-    "4": (),
-    "5": (),
-    "6": (),
-    "7": (),
-    "8": (),
-    "9": (),
-    "10": (),
-}
 
-mapa_correctas: Dict[str, Dict[str, Tuple[int, int]]] = {
-    "1": "C",
-    "2": "B",
-    "3": "A",
-    "4": "D",
-    "5": "B",
-    "6": "B",
-    "7": "A",
-    "8": "B",
-    "9": "D",
-    "10": "D",
-}
-
-cant_bordes: Dict[str, int] = {"A": 27, "B": 33, "C": 22, "D": 29}
-
-validaciones = {
-    "nombre": lambda cant_palabras: cant_palabras <= 25,
-    "date": lambda cant_palabras: cant_palabras == 8,
-    "class": lambda cant_palabras: cant_palabras == 1,
-}
+def imshow(img, new_fig=True, title=None, color_img=False, blocking=False, colorbar=True, ticks=False):
+    """
+    Imprime imágenes, con variables adecuadas.
+    """
+    if new_fig:
+        plt.figure()
+    if color_img:
+        plt.imshow(img)
+    else:
+        plt.imshow(img, cmap='gray')
+    plt.title(title)
+    if not ticks:
+        plt.xticks([]), plt.yticks([])
+    if colorbar:
+        plt.colorbar()
+    if new_fig:        
+        plt.show(block=blocking)
 
 
-def cargar_y_procesar_imagen(filepath):
+def cargar_y_procesar_imagen(filepath: str) -> Matlike:
+    """
+    Función que carga y umbraliza la imagen.
+    """
     img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
 
     _, img_bin = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY_INV)
@@ -56,109 +39,145 @@ def cargar_y_procesar_imagen(filepath):
     return img_bin
 
 
-def check_and_set(coord: Tuple[int, int, int, int]) -> str:
-    x, y, w, h = coord
 
-    if y < 45:
-        if x < 254:
-            return "nombre"
-        elif x > 290 and x < 371:
-            return "date"
-        else:
-            return "class"
-
-    if x < 270:
-        if y > 60 and y < 90:
-            return "1"
-        elif y > 170 and y < 215:
-            return "2"
-        elif y > 310 and y < 350:
-            return "3"
-        elif y > 410 and y < 480:
-            return "4"
-        elif y > 547 and y < 615:
-            return "5"
-    else:
-        if y > 60 and y < 90:
-            return "6"
-        elif y > 170 and y < 215:
-            return "7"
-        elif y > 310 and y < 350:
-            return "8"
-        elif y > 420 and y < 500:
-            return "9"
-        elif y > 547 and y < 615:
-            return "10"
-
-
-def detectar_lineas_horizontales(img_bin: np.ndarray) -> None:
+####################################################
+def detectar_lineas(img: Matlike) -> List[Tuple[int,int,int]]:
     """
-    Detecta líneas horizontales en una imagen y retorna las coordenadas de las mismas.
+    Detecta líneas horizontales utilizando morfología OPEN. Retorna los renglones ordenados.
     """
-    global mapa_subconjuntos
+    
+    lineas: Matlike = np.zeros_like(img)
 
-    kernel_horizontal = np.ones((1, 40), np.uint8)
+    kernel_horizontal: Matlike = np.ones((1, 40), np.uint8)
 
-    lineas_horizontales = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, kernel_horizontal)
+    lineas_horizontales = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel_horizontal)
 
     contornos, _ = cv2.findContours(
         lineas_horizontales, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
 
+    max_renglon = max(cv2.boundingRect(contorno)[2] for contorno in contornos)
+    renglones: List = []
+    
+
     for contorno in contornos:
-        x, y, w, h = cv2.boundingRect(contorno)
+        x, y, w, _ = cv2.boundingRect(contorno)
 
-        # Verifico si es encabezado y establezco el tamaño de los renglones
-        y = y - 19 if y <= 45 else y - 14
-        h = 17 if y <= 45 else 14
+        # Guarda solo las lineas cuya longitud sea menor a la maxima encontrada (con una tolerancia de 10 pixeles)
+        if w < max_renglon-10:
+            #cv2.line(lineas, (x, y), (x + w, y), (255, 0, 0), 2)
+            renglones.append((x,y,w))
 
-        # Verifico la anchura de la linea horizontal
-        if w < 200:
-            # cv2.rectangle(img_bin, (x, y), (x + w, y + h), (255, 255, 255), 2)
+        # Se ordenan los renglones de forma ascendiente la coordenada 'y'. En caso de que haya dos valores iguales, 
+        # se ordena de forma ascendiente la coordenada 'x'.  
+        renglones_sorted = sorted(renglones, key=lambda renglon: (renglon[1], renglon[0])) 
+    
+        #print(renglones_sorted)
+    #imshow(lineas)
+    return renglones_sorted
 
-            # Verifico y agrego al mapa de caracteres
-            key = check_and_set((x, y, x + w, y + h))
+    
+def check_and_set(renglones: List[Tuple]) -> Dict[str, Tuple[int, int, int]]:
+    """
+    Itera sobre los renglones, clasificando en: Nombre, Fecha, Clase y Preguntas de la 1 a la 10.
+    """
 
-            mapa_subconjuntos[key] = (x, y, x + w, y + h)
+    mapa_subconjuntos: Dict[str, Tuple[int, int, int]] = {
+        "Nombre": (),
+        "Date": (),
+        "Class": (),
+        "Pregunta 1": (),
+        "Pregunta 6": (),
+        "Pregunta 2": (),
+        "Pregunta 7": (),
+        "Pregunta 8": (),
+        "Pregunta 3": (),
+        "Pregunta 9": (),
+        "Pregunta 4": (),
+        "Pregunta 5": (),
+        "Pregunta 10": (),
+        }
+
+    mapa_subconjuntos["Nombre"] = renglones[0]  
+    mapa_subconjuntos["Date"] = renglones[1]    
+    mapa_subconjuntos["Class"] = renglones[2]   
+      
+    for i, key in enumerate(mapa_subconjuntos.keys()):
+        mapa_subconjuntos[key] = renglones[i]
+
+    return mapa_subconjuntos
 
 
-def contar_letras(chunk: np.ndarray) -> int:
-    count: int = 0
-    is_col: bool = True
-    _, num_columnas = chunk.shape
-
-    for col in range(num_columnas):
-        columna = chunk[:, col]
-
-        if np.count_nonzero(columna == 255) == 0:
-            if not is_col:
-                count += 1
-
-            is_col = True
+def obtener_respuesta(img: Matlike, mapa_subconjuntos: Dict[str, Tuple[int, int, int]]): 
+    """
+    Itera sobre mapa_subconjuntos para obtener las respuestas croppeadas de la imagen. 
+    """
+    respuestas = {}
+    
+    for key, (x, y, w) in mapa_subconjuntos.items():
+        
+        # Definir la altura del recorte según la clave
+        if key in ["Nombre", "Date", "Class"]:
+            h = 20  # Altura de 20 píxeles para nombre, date y class
         else:
-            is_col = False
+            h = 13  # Altura de 15 píxeles para las demás claves
+        
+        respuesta = img[y - h: y, x:x + w]
 
-    return count
-
-
-def obtener_recortes(
-    img: np.ndarray, mapa_subconjuntos: Dict[str, Tuple[int, int, int, int]]
-) -> Dict[str, Any]:
-    """
-    Esta función recibe una imagen y un mapa de bounding boxes en formato (x, y, x + w, y + h),
-    y devuelve un diccionario con los recortes de las regiones de interés.
-    """
-
-    recortes: Dict = {}
-
-    for key, (x1, y1, x2, y2) in mapa_subconjuntos.items():
-        recorte = img[y1:y2, x1:x2]
-
-        recortes[key] = recorte
-
-    return recortes
+        # Almacenar el recorte en el diccionario de respuestas
+        respuestas[key] = respuesta
+    
+    
+    # for key, recorte in respuestas.items(): 
+    #     imshow(recorte, title = key)
+        
+    return respuestas
 
 
+####################################################
+def validaciones_header(key, recorte): 
+
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(recorte, connectivity=8)
+
+    im_color = cv2.applyColorMap(np.uint8(255/num_labels*labels), cv2.COLORMAP_JET)
+    # for centroid in centroids:
+    #     cv2.circle(im_color, tuple(np.int32(centroid)), 9, color=(255,255,255), thickness=-1)
+    # for st in stats:
+        # cv2.rectangle(im_color,(st[0],st[1]),(st[0]+st[2],st[1]+st[3]),color=(0,255,0),thickness=2)
+    # imshow(img=im_color, color_img=True)
+    
+    if key == 'Nombre': 
+        
+        distancia_minima = 12
+        
+        # Cant. de letras
+        if 25 >= num_labels >= 3:
+        
+            # Se verifica la existencia de un espacio entre "Nombre" y "Apellido"
+            x_ant = stats[1][0]
+            
+            for i in range(2, num_labels):
+                x, _, _, _, _ = stats[i]
+                
+                if x - x_ant > distancia_minima:
+                    return True
+                
+                x_ant = x
+        
+        return False
+    
+    if key == 'Date':
+        if num_labels == 9: # el rectangulo del fondo suma un objeto
+            return True        
+        return False   
+    
+    if key == 'Class':
+        if num_labels == 2: # el rectangulo del fondo suma un objeto
+            return True        
+        return False
+
+
+####################################################
 def contar_pixeles(
     img_array: Matlike, umbral_min: int = 100, umbral_max: int = 200
 ) -> int:
@@ -171,72 +190,84 @@ def contar_pixeles(
     else:
         img_gray = img_array
 
-    cantidad_bordes = np.count_nonzero(img_gray)
+    cantidad_pixeles = np.count_nonzero(img_gray)
 
-    return cantidad_bordes
-
-
-def determinar_letra(cant_pixeles: int, mapa_bordes: Dict = cant_bordes) -> str:
-    for item in mapa_bordes.items():
-        letra, cant = item
-
-        if abs(cant - cant_pixeles) <= 1:
-            return letra
-
-    return "X"
+    return cantidad_pixeles    
 
 
-def plot_recortes(recortes) -> None:
-    for item in recortes.items():
-        plt.figure(figsize=(10, 5))
-        plt.title(f"Imagen: {item[0]}")
-        plt.imshow(item[1], cmap="gray")
-        plt.axis("off")
+####################################################
+def validaciones_preguntas(key: str, recorte: Matlike) -> bool: 
+    """
+    Corrige cada ejercicio. Retorna True o False
+    """
+    
+    cant_pixeles: Dict[str, int] = {"A": 28, "B": 33, "C": 22, "D": 29}
 
-        plt.show()
+    mapa_correctas: Dict[str, Dict[str, Tuple[int, int]]] = {
+        "Pregunta 1": "C",
+        "Pregunta 2": "B",
+        "Pregunta 3": "A",
+        "Pregunta 4": "D",
+        "Pregunta 5": "B",
+        "Pregunta 6": "B",
+        "Pregunta 7": "A",
+        "Pregunta 8": "B",
+        "Pregunta 9": "D",
+        "Pregunta 10": "D",
+        }
+   
+    respuesta_alumno = contar_pixeles(recorte)
+    respuesta_correcta = cant_pixeles[mapa_correctas[key]]
 
-def plot_recorte_unico(recorte) -> None:
-    plt.figure(figsize=(10, 5))
-    plt.title(f"Nombre")
-    plt.imshow(recorte, cmap="gray")
-    plt.axis("off")
-
-    plt.show()
+    if respuesta_alumno == respuesta_correcta :
+        return True
+    else:
+        return False
+    
 
 
-def definir_nota(filepath: str):
-    img: str = os.path.join(PATH_IMAGENES, filepath)
+####################################################
+# resolucion #
+####################################################
 
-    img_procesada = cargar_y_procesar_imagen(filepath=img)
 
-    detectar_lineas_horizontales(img_bin=img_procesada)
-
-    recortes = obtener_recortes(img=img_procesada, mapa_subconjuntos=mapa_subconjuntos)
-    cant_correctas: int = 0
-
-    for key, recorte in recortes.items():
-        cant_palabras: int = contar_letras(recorte)
-
-        if key in ["date", "nombre", "class"]:
-            if validaciones[key](cant_palabras):
-                print(f"{key.capitalize()}: OK")
-            else:
-                print(f"{key.capitalize()}: MAL")
-
+ 
+def definir_examen(img: Matlike):
+    renglones = detectar_lineas(img)
+    #print(renglones)
+    mapa_subconjuntos = check_and_set(renglones)
+    #print(mapa_subconjuntos)
+    respuestas = obtener_respuesta(img, mapa_subconjuntos)
+    #print(respuestas)
+    correctas: int = 0
+    examen_final = {}
+    for key, recorte in respuestas.items():
+        
+        if key in ["Nombre", "Date", "Class"]:
+            if validaciones_header(key, recorte):
+                examen_final[key] = 'OK'
+                print(f'{key}: OK')
+            else: 
+                examen_final[key] = 'MAL'
+                print(f'{key}: MAL')
             continue
 
-        borde: int = contar_pixeles(recorte)
-        letra: str = determinar_letra(borde)
-        
-        if letra == mapa_correctas[key]:
-            print(f"Pregunta {key}: OK")
-            cant_correctas = cant_correctas+1
-        else: print(f"Pregunta {key}: MAL")
-
-    return recortes['nombre'], cant_correctas
+        if validaciones_preguntas(key, recorte):
+            examen_final[key] = 'Correcto'
+            correctas += 1
+        else: 
+            examen_final[key] = 'Incorrecto'
     
-#plot_recorte_unico(recortes["nombre"])
+    examen_final['Nota'] = correctas
 
+    if correctas >= 6:
+        examen_final['Condicion'] = 'Aprobado'
+    else:
+        examen_final['Condicion'] = 'Reprobado'
+
+    return examen_final, respuestas['Nombre']
+     
+            
 def imprimir_lista_final(aprobados:List, reprobados: List) -> None:
     total_plots = len(aprobados) + len(reprobados)
 
@@ -255,22 +286,35 @@ def imprimir_lista_final(aprobados:List, reprobados: List) -> None:
     plt.tight_layout()
     plt.show()
 
+
 if __name__ == "__main__":
-    
-    examenes: Dict[int, Tuple[np.ndarray, int]] = {}
+
+    aprobados: List = []
+    reprobados: List = []
 
     for i in range(1, 6):
         relative_path: str = f'examen_{i}.png'
-        examenes[i] = definir_nota(relative_path)
-    
-    aprobados: List[np.ndarray] = []
-    reprobados: List[np.ndarray] = []
+        
+        print(f'Examen {i}: \n')
+        
+        path: str = f'examen_{i}.png'
+        img = cargar_y_procesar_imagen(os.path.join(PATH_IMAGENES, path))
+        
+        examen_final, nombre = definir_examen(img)
+        
 
-    for value in examenes.values():
-        if value[1] >= 6:
-            aprobados.append(value[0])
-        else: 
-            reprobados.append(value[0])
+        if examen_final['Nombre'] == 'MAL' or examen_final['Date'] == 'MAL' or examen_final['Class'] == 'MAL':
+            print(f'Examen {i} inválido.')
 
-    imprimir_lista_final(aprobados,reprobados)
-    
+        if examen_final['Condicion'] == 'Aprobado':
+            aprobados.append(nombre)
+        else:
+            reprobados.append(nombre)
+        for i in range(1,11):
+            print(f'Pregunta {i} :',examen_final[f'Pregunta {i}'])
+                        
+        print('------------------------------------')
+        
+    imprimir_lista_final(aprobados, reprobados)
+
+            
